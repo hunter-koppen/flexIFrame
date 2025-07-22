792980e5b4e9e9636837c518feac9e16fdea4180
Hunter Koppen <hunter.koppen@kobeon.nl>
2025-07-22 16:47:26 +0200

    finished default iframe funcionality

--- src/FlexIFrame.jsx
+++ src/FlexIFrame.jsx
@@ -1,8 +1,26 @@
 import { createElement } from "react";
 
-import { IFrameComponent } from "./components/IFrameComponent";
+import { IFrameComponent } from "./components/IFrame";
 import "./ui/FlexIFrame.css";
 
-export function FlexIFrame({ url }) {
-    return <IFrameComponent url={url} />;
+export function FlexIFrame({ url, width, height, messageToSend, messageReceived, onMessageReceived }) {
+
+    const handleMessageReceived = message => {
+        if (messageReceived) {
+            messageReceived.setValue(message);
+        }
+        if (onMessageReceived) {
+            onMessageReceived.execute(message);
+        }
+    };
+    
+    return (
+        <IFrameComponent
+            url={url.value}
+            width={width?.value || "100%"}
+            height={height?.value || "100%"}
+            messageToSend={messageToSend}
+            onMessage={handleMessageReceived}
+        />
+    );
 }
--- src/components/IFrame.jsx
+++ src/components/IFrame.jsx
@@ -1,5 +1,44 @@
-import { createElement } from "react";
+import { createElement, useRef, useEffect } from "react";
 
-export function IFrameComponent({ url }) {
-    return <iframe src={url} title="iframe" style={{ width: "100%", height: "100%", border: "none" }} />;
+export function IFrameComponent({ url, width, height, messageToSend, onMessage }) {
+    const iframeRef = useRef(null);
+
+    useEffect(() => {
+        if (messageToSend?.value && iframeRef.current && url) {
+            try {
+                const targetOrigin = new URL(url).origin;
+                iframeRef.current.contentWindow.postMessage(messageToSend.value, targetOrigin);
+                messageToSend.setValue(null);
+            } catch (error) {
+                console.error("Invalid URL provided, cannot post message:", url, error);
+            }
+        }
+    }, [messageToSend, url]);
+
+    useEffect(() => {
+        if (!url) {
+            return;
+        }
+
+        let expectedOrigin;
+        try {
+            expectedOrigin = new URL(url).origin;
+        } catch (error) {
+            // if the url is not valid, we can't check the origin, so we shouldn't listen for messages
+            console.error("Invalid URL provided to IFrameComponent:", url, error);
+            return;
+        }
+
+        const handleMessage = event => {
+            if (event.origin === expectedOrigin) {
+                if (iframeRef.current && event.source === iframeRef.current.contentWindow) {
+                    onMessage(event.data);
+                }
+            }
+        };
+
+        window.addEventListener("message", handleMessage);
+    }, [url, onMessage]);
+
+    return <iframe ref={iframeRef} src={url} title="iframe" style={{ width, height, border: "none" }} />;
 }
